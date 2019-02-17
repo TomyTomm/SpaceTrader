eaff3cc8d71a2997e6d3c720739f008b5584b90e
Thomas Tan <thomastan0617@gmail.com>
2019-02-17 14:11:52 -0500

    added variables to ConfigurationActivity.java

--- app/src/main/java/com/amath/spacetrader/view/ConfigurationActivity.java
+++ app/src/main/java/com/amath/spacetrader/view/ConfigurationActivity.java
@@ -5,6 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
+    //reference to the view model
+    private ConfigurationViewModel viewModel;
+
+    //widgets that will be getting information
+    private Spinner gameDifficultySpinner;
+    private EditText nameField;
+
+    //data for player being configured
+    private Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
