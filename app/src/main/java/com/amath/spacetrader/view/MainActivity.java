1bf5598f7135100c0d48dbe2edf0e1b45a17c9ee
Max Zuo <max.zuo@gmail.com>
2019-02-18 14:39:15 -0500

    Added Firebase SDK to project.

--- app/src/main/java/com/amath/spacetrader/view/MainActivity.java
+++ app/src/main/java/com/amath/spacetrader/view/MainActivity.java
@@ -3,9 +3,15 @@ package com.amath.spacetrader.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.amath.spacetrader.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Example code for how to set data value
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        //Example code for listening to a value. (Not completely used in this game)
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("test", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("test", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     /**
      * Button handler for the start button
+     * will be replaced with calling the repository to load game
      *
      * @param view the button that was pressed
      */
