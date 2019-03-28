aa086068813cd2109331f3afe4694546283b5a73
Thomas Tan <thomastan0617@gmail.com>
2019-03-28 16:34:23 -0400

    implement the backend fly methods with viewModel verifying canFly and the flightinteractor changing the ship fuel and location.

--- app/src/main/java/com/amath/spacetrader/viewmodel/PlanetViewModel.java
+++ app/src/main/java/com/amath/spacetrader/viewmodel/PlanetViewModel.java
@@ -4,16 +4,49 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
+import com.amath.spacetrader.entity.Planet;
+import com.amath.spacetrader.model.FlightInteractor;
 import com.amath.spacetrader.model.Model;
 import com.amath.spacetrader.model.PlanetInteractor;
 
 public class PlanetViewModel extends AndroidViewModel {
 
-    PlanetInteractor interactor;
+    PlanetInteractor planetInteractor;
+    FlightInteractor flightInteractor;
 
     public PlanetViewModel(@NonNull Application application) {
         super(application);
 
-        interactor = Model.getInstance().getPlanetInteractor();
+        planetInteractor = Model.getInstance().getPlanetInteractor();
+        flightInteractor = Model.getInstance().getFlightInteractor();
+    }
+
+    public boolean verifyFly(Planet planet, double distance) throws IllegalFlyException {
+
+        double currentFuel = flightInteractor.getRemainingFuel();
+        boolean canFly = false;
+
+        if (distance > currentFuel) {
+            throw new IllegalFlyException(String.format("You cannot fly to %s, you do not have enough fuel", planet.getName()));
+        } else {
+            canFly = true;
+            flightInteractor.flyTo(planet, distance);
+        }
+        return canFly;
+    }
+
+    private double getCurrentFuel() {
+        return flightInteractor.getRemainingFuel();
+    }
+
+    /**
+     * Please use this exception class. Look at ConfigurationViewModel
+     * as an example
+     *
+     */
+    public class IllegalFlyException extends Exception {
+        private IllegalFlyException(String message) {
+            super(message);
+        }
     }
 }
